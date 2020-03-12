35c379b4bf5f6ea31d246f7e1c2da90096d847dd
poy <poyinvest@gmail.com>
2020-03-12 13:10:47 +0100

    login page + mixin style + layout vue

--- src/components/_base-button.vue
+++ src/components/_base-button.vue
@@ -5,7 +5,18 @@
 </template>
 
 <style lang="scss" module>
-  .button {
-    background-color: $primary;
+.button {
+  @extend %typography-small;
+
+  padding: $size-button-padding;
+  color: $color-button-text;
+  cursor: pointer;
+  background: $color-button-bg;
+  border: none;
+
+  &:disabled {
+    cursor: not-allowed;
+    background: $color-button-disabled-bg;
   }
+}
 </style> 
---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <Layout>
+    <div class="about">
+      <h1>This is a login page</h1>
+      <BaseInputText
+      v-model="username"
+      name="username" 
+      :placeholder="placeholders.username"
+      >
+      </BaseInputText>
+      <BaseInputText
+      v-model="password"
+      type="password"
+      name="password" 
+      :placeholder="placeholders.password"
+      >
+      </BaseInputText>
+      <BaseButton type="submit">Sign in</BaseButton>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/views/layouts/_layout.vue";
+
+export default {
+  name: "Login",
+  components: { Layout },
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  computed: {
+    placeholders() {
+      return process.env.NODE_ENV === 'production' ? {}
+        : {
+            username: 'username by default is admin',
+            password: 'password by default is root',
+          }
+    }
+  }
+}
+</script>
